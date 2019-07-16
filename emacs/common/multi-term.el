7e5c2cc4ba95c5021c561f91413a5297d05e78f5
Randall Smith <rbsmith@adams.edu>
2019-07-16 10:28:49 -0600

    Add multi-term tweaks

--- emacs/common/multi-term.el
+++ emacs/common/multi-term.el
@@ -1,12 +1,24 @@
 ;(load-file "~/.emacs.d/multi-term.el")
 					;(el-get 'sync "multi-term")
 (package-install 'multi-term)
-;(require 'multi-term)
-;(setq multi-term-program "/bin/bash")
-(setq multi-term-program "/bin/zsh")
-(global-set-key (kbd "C-c n") 'multi-term-next)
-(global-set-key (kbd "C-c p") 'multi-term-prev)
-(global-set-key (kbd "C-c t") 'multi-term)
+(require 'multi-term)
+(setq multi-term-program "/bin/bash")
+;(setq multi-term-program "/bin/zsh")
 
-(add-hook 'term-mode-hook (lambda()
-				(setq yas-dont-activate t)))
+(setq multi-term-dedicated-select-after-open-p t)
+
+(define-prefix-command 'mt-map)
+(global-set-key (kbd "C-c t") 'mt-map)
+
+(global-set-key (kbd "C-c t n") 'multi-term-next)
+(global-set-key (kbd "C-c t p") 'multi-term-prev)
+(global-set-key (kbd "C-c t c") 'multi-term)
+(global-set-key (kbd "C-c t d") 'multi-term-dedicated-toggle)
+
+(global-set-key (kbd "C-c t x") 'term-send-M-x)
+
+(add-hook 'term-mode-hook
+	  (lambda()
+	    (define-key term-raw-map (kbd "C-z") 'self-insert-command)
+	    (setq yas-dont-activate t)
+	    ))
