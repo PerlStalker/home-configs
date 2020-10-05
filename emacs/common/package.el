682eba642a4bb1379312311e30822754684677fc
Randall Smith <rbsmith@adams.edu>
2020-10-04 20:45:34 -0600

    Fix melpa repo address

--- emacs/common/package.el
+++ emacs/common/package.el
@@ -1,7 +1,7 @@
 ;; package.el (package management)
 (require 'package)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
